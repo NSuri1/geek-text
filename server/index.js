26eb7b9640eb204c36ba849d8f8512c49869f3cc
Adrian Sanchez <skatehumor@yahoo.com>
2019-01-23 08:47:34 -0500

    Added basic server boilerplate to get started

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
+const SERVER_PORT = 30505;
+
 var app = require('express')()
-// Do something after this
+
+app.get('/', (req, res) => {
+	res.send("Hello there!");
+});
+
+app.listen(SERVER_PORT, () => {
+	console.log(`Geek-Text server now up on port ${SERVER_PORT}`);
+});

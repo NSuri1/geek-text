e1e4860ac79ac0654e5f29567d3cdd76003e194e
Daniel Valencia <valenciad1994@gmail.com>
2019-02-02 17:25:45 -0500

    Installed Material-ui and added a nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Router from './components/Router';
+import CustomAppBar from './CustomAppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+      <CustomAppBar></CustomAppBar>
         <Router />
       </div>
     );

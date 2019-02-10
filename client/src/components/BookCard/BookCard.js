441213b70174fd6686b02ddee136b159dde267f7
Macbook <nalinsuri@gmail.com>
2019-02-10 16:29:52 -0500

    Display general book info except for rating

--- client/src/components/BookCard/BookCard.js
+++ client/src/components/BookCard/BookCard.js
@@ -15,9 +15,9 @@ class BookCard extends Component {
 		api.getMedia({ id: this.props.book.cover_image }, (result) => {
 			let media = JSON.parse(result);
 			media = media.results;
-				this.setState({
-					coverImage: media.base64 || []
-				});
+			this.setState({
+				coverImage: media.base64 || []
+			});
 		});
 	}
 
@@ -31,14 +31,14 @@ class BookCard extends Component {
 		const cardClass = `book-card ${this.state.hasMouse ? 'hovered' : ''}`
 		return (
 			<div className={cardClass} onMouseEnter={this.onCardHover} onMouseLeave={this.onCardHover}>
-				<Link to={{pathname: '/book-details', state: { bookId: this.props.book._id }}}>
-					<img className="cover-image" src={this.state.coverImage !== '' ? 'data:image/jpeg;base64,' + this.state.coverImage : 'book-placeholder.jpg'} alt="Book Cover"/>
+				<Link to={{ pathname: '/book-details', state: { bookId: this.props.book._id, book: this.props.book, bookCover: this.state.coverImage } }}>
+					<img className="cover-image" src={this.state.coverImage !== '' ? 'data:image/jpeg;base64,' + this.state.coverImage : 'book-placeholder.jpg'} alt="Book Cover" />
 					{/*Use this to test the placeholder image, or delete if desired
 						<img className="cover-image" src={'book-placeholder.jpg'} alt="Book Cover"/>*/}
 				</Link>
 				<div className="book-info">
 					<h5 className="title">{this.props.book.title}</h5>
-					<Link to={{ pathname: '/', state: { bookId: this.props.book._id }}}>
+					<Link to={{ pathname: '/', state: { bookId: this.props.book._id } }}>
 						<h6 className="author">{`by ` + this.props.book.authors.reduce((acc, val) => acc + (acc ? ", " : "") + val.name, "")}
 						</h6>
 					</Link>
--- client/src/components/BookDetails/BookDetails.js
+++ client/src/components/BookDetails/BookDetails.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { api } from '../../api/ApiProvider';
+import './BookDetails.css';
 
 class BookDetails extends Component {
 	constructor(props) {
 		super(props);
+		let { state } = props.location;
 		this.state = {
-			bookId: props.location.state ? props.location.state.bookId : null,
-			book: null,
+			bookId: state ? state.bookId : null,
+			book: state ? state.book : null,
+			bookCover: state ? state.bookCover : null
 		};
 	}
 
 	componentDidMount() {
-		this.fetchBookInformation();
+		if (!this.state.book) {
+			console.log('Making network request for book info');
+			this.fetchBookInformation();
+		}
 	}
 
 	fetchBookInformation() {
@@ -30,10 +36,61 @@ class BookDetails extends Component {
 	}
 
 	render() {
+		const { book } = this.state;
+		console.log(book);
 		return (
-			<div>
-				{JSON.stringify(this.state.book)}
-			</div>
+			<div className="container">
+				<div className="imgContainer">
+					<img className="imageCover" src={this.state.bookCover !== '' ? `data:image/jpeg;base64,${this.state.bookCover}` : 'book-placeholder.jpg'} alt="Book Cover" />
+				</div>
+				<div className="bookInfoSubContainer">
+					<div>
+						<div className="title">
+							{book.title}
+						</div>
+					</div>
+					<div>
+						<div className="heading">
+							Author(s):
+						</div>
+						<div>
+							{book.authors.reduce((acc, val) => acc + (acc ? ', ' : '') + val.name, '')}
+						</div>
+					</div>
+					<div>
+						<div className="heading">
+							Ratings & Reviews:
+						</div>
+						<div>
+							3.5/5.0
+						</div>
+					</div>
+					<div>
+						<div className="heading">
+							Description:
+						</div>
+						<div>
+							{book.description}
+						</div>
+					</div>
+					<div>
+						<div className="heading">
+							Publishing Info:
+						</div>
+						<div>
+							Published {new Date(book.published_on).toLocaleDateString()} by {book.publisher}
+						</div>
+					</div>
+					<div>
+						<div className="heading">
+							Other Info:
+						</div>
+						<div>
+							ISBN: {book.isbn13}
+						</div>
+					</div>
+				</div>
+			</div >
 		);
 	}
 }

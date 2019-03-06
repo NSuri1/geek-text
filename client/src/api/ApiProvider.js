c2a0dc352d72a30f07c6770f599c0b097c8bb115
Adrian Sanchez <skatehumor@yahoo.com>
2019-03-05 19:16:05 -0500

    Another hot commit

--- client/src/api/ApiProvider.js
+++ client/src/api/ApiProvider.js
@@ -89,6 +89,12 @@ class ApiProvider {
 			if (callback) callback(body);
 		});
 	}
+
+	_partial(func, ...argsBound) {
+		return function(...args) {
+			return func.call(this, ...argsBound, ...args);
+		}
+	}
 }
 
 export const api = new ApiProvider();

092c92d655be12e854e9193ff82b02a9c6806c57
Macbook <nalinsuri@gmail.com>
2019-04-13 13:29:09 -0400

    Seems to be working

--- client/src/components/ShoppingCart/CartContainer/BookContainer/index.js
+++ client/src/components/ShoppingCart/CartContainer/BookContainer/index.js
@@ -1,4 +1,1 @@
-export {
-    default
-}
-from './BookContainer';
+export { default } from "./BookContainer";
--- client/src/components/ShoppingCart/CartContainer/CartContainer.js
+++ client/src/components/ShoppingCart/CartContainer/CartContainer.js
@@ -1,38 +1,28 @@
-import React, {
-    Component
-} from 'react';
-import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
-import './CartContainer.css';
-import BookContainer from './BookContainer/BookContainer.js';
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import "./CartContainer.css";
+import BookContainer from "./BookContainer/BookContainer.js";
 
 class CartContainer extends Component {
-
-    render() {
-        return this.props.books.map(book => ( <
-            div >
-            <
-            BookContainer key = {
-                book.book_id
-            }
-            book = {
-                book
-            }
-            handleRemoveBookFromCart = {
-                this.props.handleRemoveBookFromCart
-            }
-            handleChangeBookQuantity = {
-                this.props.handleChangeBookQuantity
-            }
-            /> <
-            /div>
-        ));
-    }
-
+  render() {
+    return this.props.books.map(book => (
+      <div key={book.book_id}>
+        <BookContainer
+          key={book.book_id}
+          book={book}
+          handleRemoveBookFromCart={this.props.handleRemoveBookFromCart}
+          handleChangeBookQuantity={this.props.handleChangeBookQuantity}
+          handleSaveForLater={this.props.handleSaveForLater}
+        />
+        <hr className="line-seperator" />
+      </div >
+    ));
+  }
 }
 
 CartContainer.propTypes = {
-    book: PropTypes.object,
-}
+  books: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default CartContainer;
